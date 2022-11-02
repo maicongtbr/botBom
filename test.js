3c0966cb7c374f8322e7e261b3f7d11dc7ddb03b
maicomgtbr <michael-daguia@hotmail.com>
2022-11-02 12:05:12 -0300

    !img disabled

--- test.js
+++ test.js
@@ -1,10 +1,29 @@
-const deepai = require('deepai');
+const pokemon = require('pokemon.js');
 
-deepai.setApiKey('quickstart-QUdJIGlzIGNvbWluZy4uLi4K');
+pokemon.setLanguage('english');
 
-(async function() {
-    var resp = await deepai.callStandardApi("text2img", {
-            text: "deyverson flamengo",
-    });
-    console.log(resp);
-})()
+//Testar com a PokeAPI, já que tem mais informações que eu preciso, como a raridade dos pokemons
+
+var PokeInfo = new Object('habilidades:', )
+
+const randomNumber = () => {
+    return Math.floor(Math.random() * (386 - 1 + 1));
+}
+
+const getRandomPokemon = async () => {
+    selectedPoke = await pokemon.getPokemon(randomNumber());
+
+    var pokeSprite = selectedPoke.sprites.front_default;
+    var pokeName = selectedPoke.name;
+    var pokeId = selectedPoke.id;
+    var pokeStats = selectedPoke.stats;
+    var pokeType = [];
+    selectedPoke.types.forEach((element) => {
+        pokeType.push(element.name);
+    })
+    
+    // console.log(await pokemon.getPokemon(259));
+    console.log('Link da Sprite: ' + pokeSprite + '\nTipo: ' + pokeType + '\nID: ' + pokeId + '\nNome: ' + pokeName + '\nStats: \n' + pokeStats);
+}
+
+getRandomPokemon();
