8f226b7c7cf15734e2869f136b5476a1cb6bf335
Ceetros <joaoraphaelpp@gmail.com>
2022-11-27 22:39:18 -0300

    git

--- Modules/Pokemon/encounter.js
+++ Modules/Pokemon/encounter.js
@@ -8,7 +8,7 @@ const isShiny = () => {
 }
 
 const getCorrectImage = (images, female, shiny) => {
-    return images[`front${shiny ? "_shiny_" : female ? "_female" : "_default"}`]
+    return images[`front${shiny ? "_shiny" : female ? "_female" : "_default"}`]
 }
 
 var encounterMessages = {
