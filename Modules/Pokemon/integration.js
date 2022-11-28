4daef691209b0cc0a86a35058fe855cd79483123
Ceetros <joaoraphaelpp@gmail.com>
2022-11-27 23:32:07 -0300

    fefewf

--- Modules/Pokemon/integration.js
+++ Modules/Pokemon/integration.js
@@ -87,7 +87,9 @@ const onMessage = async (msg) => {
         var storage = getStorageValue("pokemonModuleCurrentServerPokemon");
         var id = msg.from ? msg.from : msg.chatId;
 
-        if(!storage[id] || storage[id].catch == true) {
+        if(storage[id] || storage[id].catch == true) {
+            storage[id] = {};
+            getStorage("pokemonModuleCurrentServerPokemon").setValue(storage);
             return;
         }
 
