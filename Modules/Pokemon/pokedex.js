2fa46902ee1c31a8f37eecab066f585c5df8f40c
Ceetros <joaoraphaelpp@gmail.com>
2022-11-28 21:00:20 -0300

    fwefw

--- Modules/Pokemon/pokedex.js
+++ Modules/Pokemon/pokedex.js
@@ -1,6 +1,7 @@
 const superagent = require("superagent");
 const { MessageMedia } = require("whatsapp-web.js");
 var capitalize = require('capitalize');
+const download = require('image-downloader');
 
 const getPokedex = async (msg) => {
     var splited = msg.body.split(" ");
