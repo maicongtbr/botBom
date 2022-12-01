b94dd870613349d531e7b2ba720422d3b19551da
Ceetros <joaoraphaelpp@gmail.com>
2022-12-01 11:05:24 -0300

    fwefwf

--- Modules/Pokemon/locations.js
+++ Modules/Pokemon/locations.js
@@ -21,7 +21,7 @@ const updateLocationCache = async () => {
             for (area of areas) {
                 parsedRegions.push(await Areas.getPokemonArea(area, name.name))
             }
-            // console.log(`Carregado a location ${location.name} da regiao ${parseInt(region) + 1} (${global.regions[parseInt(region)]})`);
+            console.log(`Carregado a location ${location.name} da regiao ${parseInt(region) + 1} (${global.regions[parseInt(region)]})`);
 
         }
     }
@@ -53,8 +53,6 @@ const updateLocationCache = async () => {
     }
 
 }
-
-updateLocationCache()
 
 
 module.exports = { updateCache: async (cb) => {
