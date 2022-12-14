b80e42c31b22a6eced66197c179b7949e5118ff9
maicomgtbr <michael-daguia@hotmail.com>
2022-12-13 21:05:16 -0300

    fix

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ const sendEpicFreeGames = async (bot) => {
     var res = await getGames('BR', false)
 
     //salva o Id do jogo de graça da semana atual para a checagem do if abaixo
+    currentGameId = res.currentGames[0][0].id;
     if(!pastNextGameId) {
         pastNextGameId = res.nextGames[0][0].id;
+        return;
     }
-    currentGameId = res.currentGames[0][0].id; 
 
 
     //se o Id do jogo de graça dessa semana for o mesmo do nextGame da semana passada
