5342c75606dfb82801ebb284ddfd5b79ff682ba2
Ceetros <joaoraphaelpp@gmail.com>
2022-12-15 21:44:33 -0300

    arrayf num ta definido no database ain mesmo onde tu ta

--- database.js
+++ database.js
@@ -35,7 +35,7 @@ class Database {
             collection: 'PokemonBoxModule'
         }));
         this.connection.model('Cache', new mongoose.Schema({
-            info: Object | Arrayf,
+            info: Object | Array,
             name: String
         }, {
             collection: 'Cache'
