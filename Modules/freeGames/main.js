8fbf32de7668c9194961831a27c36751f633aa09
Ceetros <joaoraphaelpp@gmail.com>
2022-12-17 17:04:49 -0300

    date?

--- Modules/freeGames/main.js
+++ Modules/freeGames/main.js
@@ -18,7 +18,12 @@ const init = async (bot) => {
 
 const MAIN_LOOP_TIME = 60 * 60 * 1000; // 1h
 
+Date.getBrazilTime = (this) => {
+    return this.toLocaleString('pt-BR', { timeZone: "America/Sao_Paulo" })
+}
+
 const scheduleMainLoop = () => {
+    console.log(`Checando o novo jogo da epic em ${new Date(new Date() + MAIN_LOOP_TIME).getBrazilTime()}`)
     setTimeout(mainLoop, MAIN_LOOP_TIME);
 }
 
