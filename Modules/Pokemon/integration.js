c652d04c16dbf4e0ea8d162c6dc99aabfd7822e1
Ceetros <joaoraphaelpp@gmail.com>
2022-12-29 22:08:36 -0300

    log

--- Modules/Pokemon/integration.js
+++ Modules/Pokemon/integration.js
@@ -195,10 +195,11 @@ const showPokemon = async (msg) => {
         let playerInfos = { coins: player.coins, name: contact.pushname, image: await contact.getProfilePicUrl() }
         let Pokemon = [];
         for (let index = 0; index < player.pokemon.length; index++) {
-            const e = array[index];
+            const e = player.pokemon[index];
             Pokemon.push({name: e.name, level: e.level, hp: { current: e.currentHp, max: e.maxHp }, shiny: e.shiny });
-            
         }
+
+        console.log(playerInfos, Pokemon);
 
         let img = await PokeParty.getPokemonPartyImage(playerInfos, Pokemon);
         return await msg.reply(img);
