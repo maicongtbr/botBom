59715cc3c5dadc654c09aab392b1020d3258c57e
maicomgtbr <michael-daguia@hotmail.com>
2023-03-03 14:28:15 -0300

    Update callback.js

--- callback.js
+++ callback.js
@@ -47,9 +47,9 @@ const banMember = (msg, bot) => {
                     if (hasMentions){
                         msg.getMentions().then((mentionedUsers) => {
                             for (let i = 0; i <= mentionedUsers.length; i++){
+                                console.log(mentionedUsers[i].PrivateContact.id._serialized);
+                                console.log(mentionedUsers);
                                 if (mentionedUsers[i].PrivateContact.id._serialized === '5521991241118@c.us') {
-                                    console.log(mentionedUsers[i].PrivateContact.id._serialized);
-                                    console.log(mentionedUsers);
                                     console.log('mencionou o bot');
                                     msg.reply('*JAMAIS TENTE ISSO*');
                                     group.removeParticipants([msg.author]);
